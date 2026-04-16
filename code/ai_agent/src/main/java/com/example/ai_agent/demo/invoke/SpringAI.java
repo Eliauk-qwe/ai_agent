3a3150d7405600a6ae6825cd3f20a6fa57ed805c
ELiauk-qwe <1552356639@qq.com>
2026-04-16 13:24:11 +0800

    【ChatClient （Loveapp）】

--- code/ai_agent/src/main/java/com/example/ai_agent/demo/invoke/SpringAI.java
+++ code/ai_agent/src/main/java/com/example/ai_agent/demo/invoke/SpringAI.java
@@ -1,16 +1,20 @@
 package com.example.ai_agent.demo.invoke;
 
 import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Component  // 注释掉，不让它自动运行
 public class SpringAI  implements CommandLineRunner {
     @Resource
     private ChatModel  dashscopeChatModel;
+
 
 
     @Override
